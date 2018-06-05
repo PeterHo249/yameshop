12559f3c5e731dad385b260694de92e17b0eaa8f
Tran Manh Chung <chungtran0603@gmail.com>
2018-06-05 16:46:36 +0700

    load-ui

--- client_app/bussiness/present_generator.js
+++ client_app/bussiness/present_generator.js
@@ -13,7 +13,7 @@ class UI {
         return string;
     }
 
-    RenderUI(req) {
+    RenderUI(req,fivePage,numPage) {
         let layout_html = fs.readFileSync('./client_app/index_guest.html', 'utf-8');
         let content_html;
 
@@ -23,39 +23,51 @@ class UI {
                 i = 0
                 content_html = fs.readFileSync('./client_app/snippets/homepage.html', 'utf-8');
                 for (; i < 12; i++) {
-                    content_html = this.InsertProperty(content_html, 'NEW_PRODUCT_NAME_' + i, dataJson.news[i].name);
+                    content_html = this.InsertProperty(content_html, 'NEW_PRODUCT_NAME_' + i,'<a href="product?productId='+dataJson.news[i].id+'_L">' +dataJson.news[i].name+'</a>' );
                     content_html = this.InsertProperty(content_html, 'NEW_PRODUCT_PRICE_' + i, dataJson.news[i].out_price);
-                    content_html = this.InsertProperty(content_html, 'TREND_PRODUCT_NAME_' + i, dataJson.trends[i].name);
+                    content_html = this.InsertProperty(content_html, 'NEW_PRODUCT_IMAGE_' + i, '<img src="'+'/photos/AK_AD_0000_C.jpg'+'" class="img-responsive" alt="a" />');//
+                    content_html = this.InsertProperty(content_html, 'TREND_PRODUCT_NAME_' + i,'<a href="product?productId='+dataJson.trends[i].id+'_L">'+ dataJson.trends[i].name+'</a>');
                     content_html = this.InsertProperty(content_html, 'TREND_PRODUCT_PRICE_' + i, dataJson.trends[i].out_price);
+                    content_html = this.InsertProperty(content_html, 'TREND_PRODUCT_IMAGE_' + i,  '<img src="'+'/photos/AK_AD_0000_C.jpg'+'" class="img-responsive" alt="a" />');//
                 }
                 break;
             case '2':
-                i = 0
+                let strHtml = '<a href="#">{{PRODUCT_NAME_7}}</a>'
+                i = numPage*8;
                 content_html = fs.readFileSync('./client_app/snippets/product_list.html', 'utf-8');
-                for (; i < 8; i++) {
-                    content_html = this.InsertProperty(content_html, 'PRODUCT_NAME_' + i, dataJson[i].name);
-                    content_html = this.InsertProperty(content_html, 'PRODUCT_PRICE_' + i, dataJson[i].out_price);
+
+                let n = (i+8)<dataJson.length?(i+8):dataJson.length;
+                let temp = i;
+
+                let item_product='';
+
+                for (let k = 0; k < n-temp; k++,i++) {
+                    item_product+='<div class="col-sm-6 col-md-6 col-lg-4 col-xl-3"><div class="tcb-product-item"><div class="tcb-product-photo"><a href="#">'+
+                    '<img src="'+'/photos/AK_AD_0000_C.jpg'+'" class="img-responsive" alt="a" />'+'</a></div><div class="tcb-product-info"><div class="tcb-product-title"><h4>'+
+                    '<a href="product?productId='+dataJson[i].id+'_L">' + dataJson[i].name + '</a>'+'</h4></div><div class="tcb-hline"></div><div class="tcb-product-price">'+
+                    dataJson[i].out_price +'vnđ</div></div></div></div>';
                 }
+                content_html = this.InsertProperty(content_html, 'PRODUCT_LIST',item_product);
+                content_html = this.InsertProperty(content_html, 'PAGE_NUMBER', this.DisplayPageNumber(dataJson.length,fivePage,numPage));
                 break;
             case '3':
                 i = 0
-                content_html = fs.readFileSync('./client_app/snippets/product_list.html', 'utf-8');
-                for (; i < 8; i++) {
-                    content_html = this.InsertProperty(content_html, 'PRODUCT_NAME_' + i, dataJson[i].name);
-                    content_html = this.InsertProperty(content_html, 'PRODUCT_PRICE_' + i, dataJson[i].out_price);
-                }
-                break;
-            case '4':
-                i = 0
                 content_html = fs.readFileSync('./client_app/snippets/product_detail.html', 'utf-8');
-                let info_table;
+                let info_table='';
+                let other_color='';
                 for (; i < dataJson.color.length; i++) {
                     info_table += '<tr><th scope="row">'+dataJson.id +'_' + dataJson.color[i].id +'</th><td>'+dataJson.color[i].name+','+dataJson.size+
-                    '</td><td>'+dataJson.color[i].shops+'</td></tr>';
+                    '</td><td style="color : blue" class="popup" onmouseover="showFunction('+i+')" onmouseout="hideFunction('+i+')">'+dataJson.color[i].shops.length+
+                    ' Cửa hàng<span class="popuptext" id="'+i+'">'+this.ProcessDisplayListShop(dataJson.color[i].shops)+'</span></td></tr>';
+
+                    other_color += ' <div class="col-sm-6 col-md-4"><img src="'+'/photos/AK_AD_0000_C.jpg'+'" alt="hinh ao" class="product-photo"></div>';
                 }
                 content_html = this.InsertProperty(content_html, 'INFO_TABLE',info_table);
+                content_html = this.InsertProperty(content_html, 'PRODUCT_IMAGE_DIFFERENT_COLOR',other_color);
                 content_html = this.InsertProperty(content_html, 'PRODUCT_NAME',dataJson.name);
                 content_html = this.InsertProperty(content_html, 'PRODUCT_PRICE',dataJson.out_price);
+                content_html = this.InsertProperty(content_html, 'PRODUCT_IMAGE_MAIN','<img id="product-photo-sample" src="'+'/photos/AK_AD_0000_C.jpg'+'" alt="hinh mau">');
+
                 break;
         }
 
@@ -64,39 +76,31 @@ class UI {
         return layout_html;
     }
 
-    SendRequestGetData(req, firstParams, secondParams) {
-
-        let addressProcess;
-        let addressService = "http://localhost:3030";
-
-        switch (req) {
-            case '1': //get hot and trend product
-                let home_guest = "home_guest";
-                addressProcess = `${addressService}/${home_guest}`;
-                break;
-            case '2': //get info product list with two params
-                let product_list_2 = "product_list";
-                let category_2 = "category";
-                let brand_2 = "brand";
-                addressProcess = `${addressService}/${product_list_2}?${category_2}=${firstParams}&${brand_2}=${secondParams}`;
-                break;
-            case '3': //get info product list with one params
-                let product_list_1 = "product_list";
-                let category_1 = "category";
-                addressProcess = `${addressService}/${product_list_1}?${category_1}=${firstParams}`;
-                break;
-            case '4': //get info a product
-                let product = "product";
-                let productId = "productId";
-                addressProcess = `${addressService}/${product}?${productId}=${firstParams}`;
-                break;
-            default:
-                res.writeHeader(404, {
-                    'Content-Type': 'text/plain'
-                });
-                res.end("Request was not support!!!");
-                break;
+    ProcessDisplayListShop(listShop){
+        let result = '';
+        for(var i=0;i<listShop.length;i++){
+            result+=listShop[i]+'</br>';
         }
+        return result;
+    }
+    DisplayPageNumber(amountProduct,fivePageNext,numPage){
+        amountProduct = amountProduct/8;
+        let result ='';
+        let limit = (fivePageNext + 5)<=amountProduct?(fivePageNext + 5):amountProduct;
+        for(let i= fivePageNext;i<=limit;i++){
+            if(i==numPage){
+                result+='<li class="page-item active"><a class="page-link" href="/product_list_change?page='+i+'">'+i+'</a></li>';
+            }
+            else{
+                result+='<li class="page-item"><a class="page-link" href="/product_list_change?page='+i+'">'+i+'</a></li>';
+            }
+        }
+        return result;
+    }
+
+    SendRequestGetData(urlExtension) {
+
+        let addressProcess="http://localhost:3030"+urlExtension;
 
         let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
         let processHttp = new XMLHttpRequest();
--- client_app/service_client.js
+++ client_app/service_client.js
@@ -3,63 +3,104 @@
 
 /*jshint esversion: 6 */
 
-let app = require('http');
-let fs = require('fs');
-let url = require('url');
-let query = require('querystring');
-let present_generator = require('./bussiness/present_generator');
+let app = require("http");
+let fs = require("fs");
+let url = require("url");
+let query = require("querystring");
+let present_generator = require("./bussiness/present_generator");
 var port = 3000;
+let page = -5;
+let listProductLink;
 
-app.createServer((req, res) => {
-    let params = {};
-    params = url.parse(req.url, true).query;
-    // console.log('--------------------------------------' + req.method + " " + String(req.url.match(/\/\w+/)));
+app
+  .createServer((req, res) => {
 
-    let req_url = (req.url == '/') ? '/index_guest.html' : req.url;
+    let type;
+    let urlExtension;
+    let numPage = 0;
+    let flag = false;
 
-    let file_extension = req.url.lastIndexOf('.');
+    switch (String(req.url.match(/\/\w+/))) {
+      case 'null': //home_guest
+        urlExtension = "/home_guest";
+        type = "1";
+        flag=true;
+        break;
+      case '/product_list':
+        urlExtension = req.url;
+        req.url = "/product_list.html";
+        type = "2";
+        listProductLink = urlExtension;
+        if (page == -5) page += 5;
+        flag=true;
+        break;
+      case '/product': //detail
+        urlExtension = req.url;
+        req.url = "/product_detail.html";
+        type = "3";
+        flag=true;
+        break;
+      case '/product_list_next':
+        urlExtension = listProductLink;
+        req.url = "/product_list.html";
+        type = "2";
+        page += 5;
+        break;
+      case '/product_list_previous':
+        urlExtension = listProductLink;
+        req.url = "/product_list.html";
+        type = "2";
+        if (page > 0)
+          page -= 5;
+        break;
+        case '/product_list_change':
+        urlExtension = listProductLink;
+        numPage = parseInt(String(req.url.match(/\=\w+/)).substr(1,String(req.url.match(/\=\w+/)).length));
+        req.url = "/product_list.html";
+        type = "2";
+        break;
+    }
 
-    let header_type = (file_extension == -1 && req.url != '/') ? 'text/plain' : {
-        '/': 'text/html',
-        '.html': 'text/html',
-        '.ico': 'image/x-icon',
-        '.jpg': 'image/jpeg',
-        '.png': 'image/png',
-        '.gif': 'image/gif',
-        '.css': 'text/css',
-        '.js': 'text/javascript'
+    let req_url = req.url == "/" ? "/index_guest.html" : req.url;
+    let file_extension = req.url.lastIndexOf(".");
+    let header_type = file_extension == -1 && req.url != "/" ? "text/plain" : {
+      "/": "text/html",
+      ".html": "text/html",
+      ".ico": "image/x-icon",
+      ".jpg": "image/jpeg",
+      ".png": "image/png",
+      ".gif": "image/gif",
+      ".css": "text/css",
+      ".js": "text/javascript"
     }[req.url.substr(file_extension)];
 
-    if (header_type === 'text/html') {
-        // TODO - Implement code to render html file
-        switch (String(req_url.match(/(\/\w+\.\w+)/)[0])) {
-            case '/index_guest.html':
-                    res.setHeader('Content-type', header_type);
-                    //------------------------------
-                    present_generator.SendRequestGetData('4','AK_MB_0000_L');
-                    //------------------------------
-                    res.end(present_generator.RenderUI('4')); 
-                break;
-        }
+    if (header_type === "text/html") {
+      // TODO - Implement code to render html file
+      res.setHeader("Content-type", header_type);
+      if(flag==true){
+        present_generator.SendRequestGetData(urlExtension);
+      }
+      res.end(present_generator.RenderUI(type, page, numPage));
     } else {
-        fs.readFile(__dirname + req_url, (err, data) => {
-            if (err) {
-                console.log('==> Error: ' + err);
-                console.log('==> Error 404: File not found ' + res.url);
+      fs.readFile(__dirname + req_url, (err, data) => {
+        if (err) {
+          console.log("==> Error: " + err);
+          console.log("==> Error 404: File not found " + res.url);
 
-                res.writeHead(404, 'Not Found');
-                res.end();
-            } else {
-                res.setHeader('Content-type', header_type);
-                res.end(data);
-                console.log(req.url, header_type);
-            }
-        });
+          res.writeHead(404, "Not Found");
+          res.end();
+        } else {
+          res.setHeader("Content-type", header_type);
+          res.end(data);
+          console.log(req.url, header_type);
+        }
+      });
     }
-}).listen(port, (err) => {
+  })
+  .listen(port, err => {
     if (err != null) {
-        console.log('==> Error: ' + err);
+      console.log("==> Error: " + err);
     } else {
-        console.log('Server is starting at port ' + port);
+      console.log("Server is starting at port " + port);
     }
-});
+  });
